1bbc7e855a49ca48583bf34f15ff675f98b678db
kris-randen <kris.randen@gmail.com>
2022-06-07 16:42:02 +0530

    First commit after creating the Xcode project.

---
+++ instagram/instagram/instagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagramApp.swift
+//  instagram
+//
+//  Created by Krishnaswami Rajendren on 6/7/22.
+//
+
+import SwiftUI
+
+@main
+struct instagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
