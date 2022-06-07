59f7e53ffd04d9684586df099d5f83dde850c8ed
kris-randen <kris.randen@gmail.com>
2022-06-07 16:56:08 +0530

    Organized the app into Models Views ViewModels APIs and Utils structure. And categorized the Views into Main, UploadPost, Profile, Feed, Comments, Search, Notifications, Authentication, Items etc...

---
+++ instagram/instagram/Views/Main/HomeTabView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeTabView.swift
+//  instagram
+//
+//  Created by Krishnaswami Rajendren on 6/7/22.
+//
+
+import SwiftUI
+
+struct HomeTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeTabView()
+    }
+}
