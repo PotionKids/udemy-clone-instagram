fa8104d37d284f2185b0f08a4d2a462d455b3000
kris-randen <kris.randen@gmail.com>
2022-06-08 23:17:09 +0530

    Trivial.

--- instagram/instagram/Utils/Labels.swift
+++ instagram/instagram/Utils/Labels.swift
@@ -52,6 +52,7 @@ struct Labels {
     }
     
     
+    
     static func profilePic(for user: User, withScaling scaling: CGFloat) -> some View {
         Image(user.image)
             .resizable()
