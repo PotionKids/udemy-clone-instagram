28d6ef984734aa19ae18e44a2d2b4a45f86b9192
kris-randen <kris.randen@gmail.com>
2022-06-18 01:16:18 +0530

    Fixed the jumpy loading of Feed View. This was happening because in the Content View after unwrapping the current user from the auth view model using an if let I was loading the Feed View in the else clause. So till the user was fetched it had a different feed view and then when the user is fetched the Feed View gets reshuffled because it's randomized. I removed the Feed View from the else clause and this prevents the jumpy loading of the feed.

--- instagram/instagram/Views/Main/ContentView.swift
+++ instagram/instagram/Views/Main/ContentView.swift
@@ -17,8 +17,6 @@ struct ContentView: View {
             } else {
                 if let user = viewModel.user {
                     HomeTabView(user: user)
-                } else {
-                    FeedView()
                 }
             }
         }
